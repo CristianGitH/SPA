dd6172f74f3edfee3e5d1de84a93dd92302ddb57
davinchi480 <jdgallego777@gmail.com>
2022-04-02 21:33:21 -0500

    Lista Proyectos Y creacion De proyectos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProyectosListaComponent } from './component/proyectos-lista/proyectos-lista.component';
+import { ProyectosSaveComponent } from './component/proyectos-save/proyectos-save.component';
+const routes: Routes = [
+  { path: 'proyectos-list', component: ProyectosListaComponent},
+  { path: 'proyectos-save', component: ProyectosSaveComponent}
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/component/proyectos-save/proyectos-save.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Proyectos } from 'src/app/domain/proyectos';
+import { ProyectosService } from 'src/app/service/proyectos.service';
+
+@Component({
+  selector: 'app-proyectos-save',
+  templateUrl: './proyectos-save.component.html',
+  styleUrls: ['./proyectos-save.component.css']
+})
+export class ProyectosSaveComponent implements OnInit {
+
+
+  public proyecto!: Proyectos;
+
+  public showMsg: boolean = false;
+  public msg!: string;
+  public type!: string;
+  
+  constructor(public proyectoService: ProyectosService,
+    private router: Router)  { }
+
+  ngOnInit(): void {
+    this.proyecto = new Proyectos(0, '','','',);
+  }
+    public save(){
+
+      console.log(this.proyecto);
+
+      this.proyectoService.save(this.proyecto).subscribe(data =>{
+      this.router.navigate(['/proyectos-list']);
+      }, error => {
+        console.log(error);
+        this.showMsg = true;
+        this.msg = 'An error has ocurred in the procedure';
+        this.type = 'danger';
+      });
+    }
+  }
+
