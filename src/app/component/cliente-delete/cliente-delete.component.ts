9229a589ac10a36fd97d4a3b8077ebddb333f7da
CarlosPolanco24 <42209540+CarlosPolanco24@users.noreply.github.com>
2022-04-18 18:53:11 -0500

    Se repara error de '_value'

--- src/app/component/cliente-delete/cliente-delete.component.ts
+++ src/app/component/cliente-delete/cliente-delete.component.ts
@@ -26,8 +26,11 @@ export class ClienteDeleteComponent implements OnInit {
   }
 
   public getById() {
-    let param = this.activatedRoute.params['_value'];
-    this.id = param.id;
+
+    //let param = this.activatedRoute.param['_value'];
+    //this.id = param.id;
+    let param = this.activatedRoute.snapshot.paramMap.get('id');    
+    this.id = Number(param); 
 
     this.clienteService.getById(this.id).subscribe(data => {
       this.cliente = data;
