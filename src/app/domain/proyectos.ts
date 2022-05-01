5035e916a233ba74eaf1b518d37d342204e5702c
CristianGitH <39308048+CristianGitH@users.noreply.github.com>
2022-04-30 21:45:55 -0500

    Se realiza la creación del componente para editar proyectos, se realiza ajustes en los componentes de crear y la lista de proyectos, se ajustan conforme al modelo que debe ser mapeado con el BackEnd, se ajustan servicios para que consuman la API.

--- src/app/domain/proyectos.ts
+++ src/app/domain/proyectos.ts
@@ -1,7 +1,11 @@
-export class Proyectos {
-    constructor(public ProyectosID: number,
-        public Titulo: string,
-        public Cliente: string,
-        public Requerimiento: string ){}
-    
+export class Project {
+  constructor(
+    public CustomerId: number,
+    public StateId: number,
+    public ProjectId: number,
+    public Names: string,
+    public StartDate: Date,
+    public FinishDate: Date,
+    public Price: number,
+    public AmountHours: number,) { }
 }
